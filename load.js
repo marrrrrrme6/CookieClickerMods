594f93ebbdbcf167bb67e003afc6aee0a4188717
marrrrrrme6 <70988388+marrrrrrme6@users.noreply.github.com>
2023-12-26 14:35:30 +0900

    Update load.js

--- load.js
+++ load.js
@@ -1,5 +1,5 @@
 !function(a) {
     for (let i = 0; i < a.length; i++) {
-        Game.LoadMod('http://marrrrrrme6.github.io/CookieClickerMods/' + a[i] + '/main.js');
+        Game.LoadMod('https://marrrrrrme6.github.io/CookieClickerMods/' + a[i] + '/main.js');
     }
 }(['CCSE','cookie-percentage-graph','GoldenMemory','bulkBuy50','bulkBuyX','ReloadGameButton','cws-betterGoldenAchievements','CookieAssistant','StockAssistant','IdleTrading','autoSacrifice']);
