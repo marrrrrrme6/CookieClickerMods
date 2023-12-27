d1e8a2067ce67c784e138186033e54fd5d25b41d
marrrrrrme6 <70988388+marrrrrrme6@users.noreply.github.com>
2023-12-27 09:03:47 +0900

    Create load2.js

---
+++ load2.js
@@ -0,0 +1,15 @@
+!async (a) => {
+  for (let url of a) {
+    try {
+        await new Promise((resolve, reject) => {
+            Game.LoadMod('https://marrrrrrme6.github.io/CookieClickerMods/' + url + '/main.js');
+        })
+      } catch (err) {
+            // エラーハンドリングをここで行うことができます
+            // または、エラーを外部に伝播させるために再度throwすることもできます
+            throw err
+      }
+  }
+  // ループが完全に終了した後に実行されます
+  console.log('ループ終了！')
+}(['CCSE','cookie-percentage-graph','GoldenMemory','bulkBuy50','bulkBuyX','ReloadGameButton','cws-betterGoldenAchievements','CookieAssistant','StockAssistant','IdleTrading','autoSacrifice']);
